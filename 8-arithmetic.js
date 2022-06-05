099b72a2d64dc892da955f5e63fb5cf3386d3aaa
CBBell99 <chrisbeckertonbell@gmail.com>
2022-06-04 23:16:44 -0600

    arithmetic solution

---
+++ 8-arithmetic.js
@@ -0,0 +1,14 @@
+function arithmetic(a, b, operator) {
+  //your code here!
+  if (operator == "add") {
+    return a + b;
+  }
+  if (operator == "subtract") {
+    return a - b;
+  }
+  if (operator == "multiply") {
+    return a * b;
+  } else {
+    return a / b;
+  }
+}
