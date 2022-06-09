7b035c250834f39bc9c90175d6d570cef2b6d6ce
CBBell99 <chrisbeckertonbell@gmail.com>
2022-06-09 16:03:22 -0700

    refactor lovefunc

--- 8-loveFunc.js
+++ 8-loveFunc.js
@@ -1,9 +1,6 @@
 function lovefunc(flower1, flower2) {
   // moment of truth
-  return (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
-    (flower1 % 2 !== 0 && flower2 % 2 === 0)
-    ? true
-    : false;
+  return flower1 % 2 !== flower2 % 2;
 }
 
 console.log(lovefunc(1, 4));
