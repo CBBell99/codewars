7a6cd4e1c17121bd73fe4750a4d251b3650d5f90
CBBell99 <chrisbeckertonbell@gmail.com>
2022-06-14 08:38:39 -0700

    round5 solution

---
+++ 7-round5.js
@@ -0,0 +1,8 @@
+function roundToNext5(n) {
+  return Math.ceil(n / 5) * 5;
+}
+
+console.log(roundToNext5(0));
+console.log(roundToNext5(2));
+console.log(roundToNext5(3));
+console.log(roundToNext5(12));
