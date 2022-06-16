a5067ebaf7083dc51f394c03fad95560e493e801
CBBell99 <chrisbeckertonbell@gmail.com>
2022-06-16 10:35:51 -0700

    findUniq solution

---
+++ 6-findUniq.js
@@ -0,0 +1,7 @@
+function findUniq(arr) {
+  return arr.find((n) => arr.indexOf(n) === arr.lastIndexOf(n));
+}
+
+console.log(findUniq([1, 0, 0]));
+console.log(findUniq([0, 1, 0]));
+console.log(findUniq([1, 1, 1, 2, 1, 1]));
