b6f9b5978ffb141ea055b5abf894e22623c048e3
CBBell99 <chrisbeckertonbell@gmail.com>
2022-06-21 23:01:18 -0700

    refactor flatten and sort array

--- 7-flatten-and-sort.js
+++ 7-flatten-and-sort.js
@@ -1,11 +1,3 @@
 function flattenAndSort(array) {
   return [].concat(...array).sort((a, b) => a - b);
 }
-
-console.log(
-  flattenAndSort([
-    [3, 2, 1],
-    [7, 9, 8],
-    [6, 4, 5],
-  ])
-);
