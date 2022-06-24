bdac3d5fd0a29b97b984130fe56c3d13477b40ba
CBBell99 <chrisbeckertonbell@gmail.com>
2022-06-24 03:39:29 -0700

    refactor basic math

--- 8-basic-math.js
+++ 8-basic-math.js
@@ -1,11 +1,3 @@
 function basicOp(operation, value1, value2) {
-  if (operation === "+") {
-    return (value1 += value2);
-  } else if (operation === "-") {
-    return (value1 -= value2);
-  } else if (operation === "*") {
-    return (value1 *= value2);
-  } else if (operation === "/") {
-    return (value1 /= value2);
-  }
+  return eval(value1 + operation + value2);
 }
