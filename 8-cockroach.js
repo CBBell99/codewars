9f7edf39d30a1225a32a0d9624730abd0be03c0f
CBBell99 <chrisbeckertonbell@gmail.com>
2022-06-25 04:28:28 -0700

    refactor solution

--- 8-cockroach.js
+++ 8-cockroach.js
@@ -1,6 +1,6 @@
 function cockroachSpeed(s) {
   let kmPerSecond = s / 60 / 60;
-  return Math.round(kmPerSecond * 100000);
+  return Math.floor(kmPerSecond * 100000);
 }
 
 console.log(cockroachSpeed(1.08));
