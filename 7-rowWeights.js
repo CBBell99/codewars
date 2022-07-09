e5c2c5c81f77573a932d323950bb0aa78e08c1db
CBBell99 <chrisbeckertonbell@gmail.com>
2022-07-09 05:44:34 -0700

    refactor row weights solution

--- 7-rowWeights.js
+++ 7-rowWeights.js
@@ -1,6 +1,6 @@
 function rowWeights(array) {
-  let t1 = array.filter((x, i) => i % 2 == 0).reduce((a, item) => a + item, 0);
-  let t2 = array.filter((x, i) => i % 2 != 0).reduce((a, item) => a + item, 0);
+  let t1 = array.filter((x, i) => i % 2 === 0).reduce((a, item) => a + item, 0);
+  let t2 = array.filter((x, i) => i % 2 !== 0).reduce((a, item) => a + item, 0);
 
   return [t1, t2];
 }
