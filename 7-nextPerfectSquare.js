9c8f52c35379e3e9ed13ed68bae558123edabaa3
CBBell99 <chrisbeckertonbell@gmail.com>
2022-07-21 07:02:14 -0700

    perfect square refactor

--- 7-nextPerfectSquare.js
+++ 7-nextPerfectSquare.js
@@ -1,12 +1,3 @@
 function findNextSquare(sq) {
-  // Return the next square if sq is a perfect square, -1 otherwise
-  let num;
-  let nextNum;
-  if (Math.sqrt(sq) % 1 === 0) {
-    num = Math.sqrt(sq);
-    nextNum = num + 1;
-  } else {
-    return -1;
-  }
-  return nextNum * nextNum;
+  return Math.sqrt(sq) % 1 ? -1 : Math.pow(Math.sqrt(sq) + 1, 2);
 }
