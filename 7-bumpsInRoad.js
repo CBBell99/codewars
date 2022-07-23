3a82d1c191ef02bbcfb5eac998ac8633be064ae2
CBBell99 <chrisbeckertonbell@gmail.com>
2022-07-23 04:02:54 -0700

    bumps in road refactor

--- 7-bumpsInRoad.js
+++ 7-bumpsInRoad.js
@@ -1,10 +1,3 @@
 function bump(road) {
-  let count = 0;
-
-  for (let i = 0; i < road.length; i++) {
-    if (road[i] === "n") {
-      count++;
-    }
-  }
-  return count <= 15 ? "Woohoo!" : "Car Dead";
+  return road.split("n").length - 1 <= 15 ? "Woohoo!" : "Car Dead";
 }
