b09de38a5be39eb29733652f4c7ddbd147402299
CBBell99 <chrisbeckertonbell@gmail.com>
2022-07-26 06:33:28 -0700

    open or senior solution

---
+++ 7-categorizeNewMewmber.js
@@ -0,0 +1,11 @@
+function openOrSenior(data) {
+  let result = [];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][0] > 54 && data[i][1] > 7) {
+      result.push("Senior");
+    } else {
+      result.push("Open");
+    }
+  }
+  return result;
+}
