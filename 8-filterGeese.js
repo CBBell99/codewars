b255a1e50440fe187d181eb748a5f7bcadca2848
CBBell99 <chrisbeckertonbell@gmail.com>
2022-07-30 04:01:15 -0700

    filter geese solution

---
+++ 8-filterGeese.js
@@ -0,0 +1,11 @@
+function gooseFilter(birds) {
+  const geese = [
+    "African",
+    "Roman Tufted",
+    "Toulouse",
+    "Pilgrim",
+    "Steinbacher",
+  ];
+
+  return birds.filter((bird) => !geese.includes(bird));
+}
