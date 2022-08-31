b6d300db49f01df10c5e2ac7445549b9eab3a795
CBBell99 <chrisbeckertonbell@gmail.com>
2022-08-31 15:14:49 -0700

    define card suit solution

---
+++ 8-defineCardSuit.js
@@ -0,0 +1,6 @@
+function defineSuit(card) {
+  if (card.includes("♣")) return "clubs";
+  if (card.includes("♦")) return "diamonds";
+  if (card.includes("♥")) return "hearts";
+  if (card.includes("♠")) return "spades";
+}
