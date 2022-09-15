4f819e1b68cacfd868131f7592168b06a59b003f
CBBell99 <chrisbeckertonbell@gmail.com>
2022-09-15 15:34:00 -0700

    refactor dashitize solution

--- 6-dashatizeIt.js
+++ 6-dashatizeIt.js
@@ -1,8 +1,8 @@
 function dashatize(num) {
-  return num
-    .toString()
-    .replace(/([13579])/g, "-$1-") // dashes around odd digits
-    .replace(/\-+/g, "-") // replace multiple dashes by one dash
-    .replace(/^\-/, "") // remove starting dash
-    .replace(/\-$/, ""); // remove ending dash
+  return isNaN(num)
+    ? "NaN"
+    : num
+        .toString()
+        .match(/([13579]|[02468]+)/g)
+        .join("-");
 }
