a969cbb28e54de42ea708db68cc90a1d331d4062
CBBell99 <chrisbeckertonbell@gmail.com>
2022-09-19 04:54:58 -0700

    refactor highest rank solution

--- 6-highestRank.js
+++ 6-highestRank.js
@@ -1,15 +1,6 @@
-const highestRank = (arr) =>
-  [
-    ...arr
-      .reduce((acc, cur) => {
-        acc.set(cur, (acc.get(cur) || 0) + 1);
-        return acc;
-      }, new Map())
-      .entries(),
-  ].sort((a, b) => {
-    if (a[1] !== b[1]) {
-      return b[1] - a[1];
-    }
-
-    return b[0] - a[0];
-  })[0][0];
+function highestRank(arr) {
+  return arr.sort(
+    (a, b) =>
+      arr.filter((i) => i === b).length - arr.filter((i) => i === a).length
+  )[0];
+}
