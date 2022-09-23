26a9edf151ba42ee82b956ea9e51626048c528a1
CBBell99 <chrisbeckertonbell@gmail.com>
2022-09-23 04:27:24 -0700

    refactor mumbling solution

--- 7-mumbling.js
+++ 7-mumbling.js
@@ -1,14 +1,6 @@
 function accum(s) {
-  let arr = [];
-  for (let i = 0; i < s.length; i++) {
-    arr.push(format(s[i], i + 1));
-  }
-  return arr.join("-");
-}
-function format(s, num) {
-  let letter = s.toUpperCase();
-  while (letter.length !== num) {
-    letter += s.toLowerCase();
-  }
-  return letter;
+  return s
+    .split("")
+    .map((x, index) => x.toUpperCase() + Array(index + 1).join(x.toLowerCase()))
+    .join("-");
 }
