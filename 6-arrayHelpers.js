ec4239302f24cc2112bbb7a2b83dc9c33685c876
CBBell99 <chrisbeckertonbell@gmail.com>
2022-10-10 08:49:04 -0700

    array helpers solution

--- 6-arrayHelpers.js
+++ 6-arrayHelpers.js
@@ -3,3 +3,28 @@ Array.prototype.square = function () {
     return n * n;
   });
 };
+Array.prototype.cube = function () {
+  return this.map(function (n) {
+    return n * n * n;
+  });
+};
+Array.prototype.average = function () {
+  return this.sum() / this.length;
+};
+Array.prototype.sum = function () {
+  return this.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+};
+Array.prototype.even = function () {
+  return this.filter(function (item) {
+    return 0 == item % 2;
+  });
+};
+Array.prototype.odd = function () {
+  return this.filter(function (item) {
+    return 0 != item % 2;
+  });
+};
+
+console.log(Array.prototype.square([2, 4]));
